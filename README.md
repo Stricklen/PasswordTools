7ade918740fe791f21c474351e96df14425cc0f2
Stricklen <77753627+Stricklen@users.noreply.github.com>
2021-04-04 12:48:35 +0100

    gui update

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ The app has an a selections fields to choose the style of generated password.
 It also has a strength calculator that gives a response "Weak", "Strong".
 
 # Future
-Implement a "moderate" and maybe "Very Strong" password strengths
-The app's generate feature creates only "Strong" or higher passwords
+Implement a "moderate" and maybe "Very Strong" password strengths.
+The app's generate feature creates only "Strong" or higher passwords.
+Add a 'copy to clipboard' button for the generated password.
--- passwordtoolsmain.py
+++ passwordtoolsmain.py
@@ -13,6 +13,7 @@ def main():
     root.title("Password Tools")
     # root.geometry("300x450")
     titlefont = ("times", 20, "bold")
+    entryfont = ("times", 15)
 
     # Create app title
     title_label = Label(root,
@@ -26,14 +27,21 @@ def main():
     # Create label frame for generation options
     generator_labelframe = LabelFrame(root, text="Generator")
     generator_labelframe.grid(row=1,column=0)
-    # Creating option menu
+    # Creating option menu and label
+    length_label = Label(generator_labelframe,
+                        text="Password Length:")
+    length_label.grid(row=0,column=0)
     var1 = StringVar()
     pass_length = OptionMenu(generator_labelframe, 
                             var1,
                             "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
                             )
     pass_length.config(text="Length")
-    pass_length.grid(row=0,column=0, rowspan=4)
+    pass_length.grid(row=1,column=0)
+    # Create 'Generate' button
+    generator_button = Button(generator_labelframe,
+                                text="Generate!")
+    generator_button.grid(row=2,column=0,rowspan="2")
     # Creating the three checkboxes
     s_var = IntVar()
     symbols_box = Checkbutton(generator_labelframe,
@@ -57,14 +65,24 @@ def main():
     lowers_box.grid(row=3,column=1)
     # Create field for generated password
     generator_output = Entry(generator_labelframe,
-                                width="40")
+                                width="25",
+                                font=entryfont)
     generator_output.grid(row=4,column=0,columnspan=2)
 
+
     # Create label frame from strength tester
-    strength_labelframe = LabelFrame(root)
+    strength_labelframe = LabelFrame(root,
+                                    text="Strength Tester")
     strength_labelframe.grid(row=2,column=0)
-
-
+    # Entry for password strength checker
+    strength_entry = Entry(strength_labelframe,
+                            width="25",
+                            font=entryfont)
+    strength_entry.pack()
+    # Label for result of the strength test
+    result_label = Label(strength_labelframe,
+                        text="RESULTS ARE IN: YOU ARE WEAK")
+    result_label.pack()
 
     close_button = Button(root,
                           text="Close",
